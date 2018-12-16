49e883510e088fa0631a53d4e718de1bdd494ae7
Lucas Meirelles <tutamc@hotmail.com>
2018-12-16 12:49:09 +0100

    Updating ML. Installed ALE 5 and studying how to implement.

--- MACHINE_LEARNING/ALE/test.cpp
+++ MACHINE_LEARNING/ALE/test.cpp
@@ -1,9 +1,14 @@
 #include "/home/meirelles/Clases_UA/MACHINE_LEARNING/ALE/Arcade-Learning-Environment/src/ale_interface.hpp"
 
-ALEInterface ale;
+ALEInterface alek;
 
-ale.setInt("random_seed", 0); // Difiere de la semilla de srand.
-ale.setFloat("repeat_action_probability", 0);
-ale.setBool("display_screen", false);
-ale.setBool("sound", false);
-ale.loadROM("/home/meirelles/Clases_UA/MACHINE_LEARNING/atari_roms_supported/demon_attack.bin");
+int main(int argc, char **argv) {
+
+	alek.setInt("random_seed", 0); // Difiere de la semilla de srand.
+	alek.setFloat("repeat_action_probability", 0);
+	alek.setBool("display_screen", false);
+	alek.setBool("sound", false);
+	alek.loadROM("/home/meirelles/Clases_UA/MACHINE_LEARNING/atari_roms_supported/demon_attack.bin");
+
+	return 0;
+}
